1f6993706a0d36f26310b02bd0479d06c512a2a7
rohyapawar2912 <125944686+rohyapawar2912@users.noreply.github.com>
2023-04-14 11:17:33 +0530

    Add

---
+++ Contact.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9
+{
+    internal class Contact
+    {
+    public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        public Contact(string firstName, string lastName, string email, string phone)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Phone = phone;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}, Email: {Email}, Phone: {Phone}";
+        }
+    }
+
+    class AddressBook
+    {
+        private List<Contact> contacts;
+
+        public AddressBook()
+        {
+            contacts = new List<Contact>();
+        }
+
+        public void AddContact(Contact contact)
+        {
+            contacts.Add(contact);
+        }
+
+        public void RemoveContact(Contact contact)
+        {
+            contacts.Remove(contact);
+        }
+
+        public void DisplayContacts()
+        {
+            if (contacts.Count == 0)
+            {
+                Console.WriteLine("No contacts found.");
+            }
+            else
+            {
+                Console.WriteLine("Contacts:");
+                foreach (var contact in contacts)
+                {
+                    Console.WriteLine(contact.ToString());
+                }
+            }
+        }
+    }
+
+}
+
